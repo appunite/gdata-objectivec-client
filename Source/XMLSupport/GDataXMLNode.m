e6effa5a6edda26ee6f316ab3968df01eee9367a
Karol Wojtaszek <karol@appunite.com>
2012-07-30 11:43:26 +0200

    Fix compilation error on new gcc

--- Source/XMLSupport/GDataXMLNode.m
+++ Source/XMLSupport/GDataXMLNode.m
@@ -767,10 +767,6 @@ static xmlChar *SplitQNameReverse(const xmlChar *qname, xmlChar **prefix) {
 
             int result = xmlXPathRegisterNs(xpathCtx, prefix, nsPtr->href);
             if (result != 0) {
-#if DEBUG
-              NSCAssert1(result == 0, @"GDataXMLNode XPath namespace %@ issue",
-                        prefix);
-#endif
             }
           }
         }
